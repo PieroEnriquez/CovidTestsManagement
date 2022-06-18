4ba4b1caefd20e3b37e03847faaf0b366b1e98d2
Piero Enriquez <pieroenriquezvera@gmail.com>
2022-06-18 13:32:33 -0500

    Adding an apply feature for the clinics and the factory for new clinics' smart contract generation

--- WHO_contract.sol
+++ WHO_contract.sol
@@ -12,10 +12,10 @@ contract WHO_contract{
     }
 
     //Mapping to relate the clinics with the validation given by the WHO
-    mapping(address => bool) validClincs;
+    mapping(address => bool) validClinics;
 
     //Mapping to relate a clinic to its contract
-    mapping(address => address) clinicContract;
+    mapping(address => address) clinicsContracts;
 
     //Addresses' array for the contracts of validated clinics
     address[] public contractsValidatedClinics;
@@ -29,10 +29,47 @@ contract WHO_contract{
     event newContract(address, address);
 
     //Modifier for only WHO to run a function
-    modifier onloWHO(address _who){
+    modifier onlyWHO(address _who){
         require(_who == WHO, "You are not allowed to run this function");
         _;
     }
 
+    //Function to apply for accessing the clinic system
+    function applyAccess() public{
+        requests.push(msg.sender);
+        emit requestAccess(msg.sender);
+    }
+
+    //Function to visualize the addresses that applied
+    function seeApplies() onlyWHO(msg.sender) public view returns(address[]memory){
+        return requests;
+    }
+
+    //Function to enable a clinic for auto management. For so, we need to set the clinic validation to true and emit the validation event
+    function newClinic() public onlyWHO(msg.sender){
+        validClinics[msg.sender] == true;
+        emit newValidatedClinic(msg.sender);
+    }
+
+    /*
+    Now we need to create a factory to generate a new contract for each validated clinic by filtering through the mapping
+    Finally, we relate the clinic's address with their contract's address and save it in the mapping
+    */
+    function clinicFactory() public{
+        require(validClinics[msg.sender] == true, "You are not a validated clinic");
+        address clinicContract = address (new Clinic(msg.sender));
+        clinicsContracts[msg.sender] = clinicContract;
+    }
+
+
+}
+
+contract Clinic{
+
+    address public clinicContract;
+
+    constructor(address _address){
+        clinicContract = _address;
+    }
 }
 
