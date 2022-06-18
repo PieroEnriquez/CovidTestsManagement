4011092b89e4b8534762bafa28b4445b7d8b0b95
Piero Enriquez <pieroenriquezvera@gmail.com>
2022-06-18 00:38:07 -0500

    Adding the events to call and a modifier for only WHO to run a function

--- WHO_contract.sol
+++ WHO_contract.sol
@@ -17,8 +17,22 @@ contract WHO_contract{
     //Mapping to relate a clinic to its contract
     mapping(address => address) clinicContract;
 
-    //Addresses array for the contracts of validated clinics
+    //Addresses' array for the contracts of validated clinics
     address[] public contractsValidatedClinics;
+
+    //Addresses' array for requests from clinics to be part of the system
+    address[] requests;
+
+    //Events
+    event requestAccess(address);
+    event newValidatedClinic(address);
+    event newContract(address, address);
+
+    //Modifier for only WHO to run a function
+    modifier onloWHO(address _who){
+        require(_who == WHO, "You are not allowed to run this function");
+        _;
+    }
 
 }
 
